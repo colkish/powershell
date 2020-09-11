6c059b508fe012e1a17bd7a53eae9a1400d0bd54
Colin Kish <ckish@hublsoft.com>
2020-09-11 09:06:50 +0100

    DTP 11 Sep 2020

--- DBATools/CopyTables/LoopThroughArrayWithQueryProc.ps1
+++ DBATools/CopyTables/LoopThroughArrayWithQueryProc.ps1
@@ -5,3 +5,5 @@ foreach ($i in $Results)
 	$Query = "select * from " + $i[0] + " where AccountID = " + $i[1]
 	Copy-DbaDbTableData -SqlInstance HSYOLAP02\SQL2K16 -Destination HSYOLAP02 -Database Test -Table $Table -Query $Query -CheckConstraints
 }
+#Results are printed even on a pk warning failure
+$Results ;
