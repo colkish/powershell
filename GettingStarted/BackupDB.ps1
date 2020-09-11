51ae918d8e9272d28ab5a23ff4da1b74fd45dbc4
Colin Kish <ckish@hublsoft.com>
2020-09-11 17:24:33 +0100

    11 Sep 2020 PDP Final for today!

--- GettingStarted/BackupDB.ps1
+++ GettingStarted/BackupDB.ps1
@@ -1,2 +1,2 @@
-Backup-DbaDatabase -SqlInstance HSYOLAP02\SQL2K16 -Path C:\temp -Database TEST_DEV_SDS_Test -Type Full
+Backup-DbaDatabase -SqlInstance HSYOLAP02\SQL2K16 -Path C:\temp -Database CISCO_TEST_SDS_DataStore -Type Full
 exit
