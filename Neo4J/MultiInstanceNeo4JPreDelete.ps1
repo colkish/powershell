e0c32ac0873fdd604d9848f7269483e50c320bc1
Colin Kish <ckish@hublsoft.com>
2020-09-25 14:13:49 +0100

    25 Sep 2020

--- Neo4J/MultiInstanceNeo4JPreDelete.ps1
+++ Neo4J/MultiInstanceNeo4JPreDelete.ps1
@@ -6,8 +6,8 @@ try
 	$neo4JBolt = "bolt://localhost:7687" 
 	$neo4JUser = "neo4j"
 	$neo4JPass = "Ts0rg123"
-	$NeoCommand = Invoke-DbaQuery -SqlInstance $SourceServer -Database $DataStoreDB -CommandType StoredProcedure -Query "maintenance.uspGetNeo4JToBeDeleteCount" -EnableException # Get Neo count of deleted account nodes and relationships command
-	$cypherCmd = $NeoCommand[0] # set cypher command to the 1st row returned from DB proc call (there will only be ever 1 row)
+	$NAccountList = Invoke-DbaQuery -SqlInstance $SourceServer -Database $DataStoreDB -CommandType StoredProcedure -Query "maintenance.uspGetNeo4JToBeDeleteCount" -EnableException # Get Neo count of deleted account nodes and relationships command
+	$cypherCmd = "CALL db.labels() YIELD label WHERE" + $NAccountList[0] + "CALL apoc.cypher.run(''MATCH (:''+label+'') RETURN count(*) as count'',{}) YIELD value RETURN label, value.count ORDER BY label ;" # set cypher command (there will only be ever 1 row returned from the proc call)
 	$cypherCmd # display command
 	cd $cypherShellLoc # change dir to cypher shell location
 	$cypherResults = ./cypher-shell.bat -a $neo4JBolt -u $neo4JUser -p $neo4JPass $cypherCmd --format verbose # make Neo4J command call 
